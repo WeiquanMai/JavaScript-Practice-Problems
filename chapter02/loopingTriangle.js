5bd6ea1488e9affeeae95b77383f929993b61bb1
WeiquanMai <weiquanmai@gmail.com>
2025-10-06 12:09:00 -0400

    Create loopingTriangle.js

---
+++ chapter02/loopingTriangle.js
@@ -0,0 +1,15 @@
+/*
+    Exercise: Looping A Triangle
+    Write a loop that makes seven calls to console.log to output the following triangle:
+    #
+    ##
+    ###
+    ####
+    #####
+    ######
+    #######
+*/
+
+for(let line = "#"; line.length <= 7; line += "#"){
+    console.log(line);
+}
